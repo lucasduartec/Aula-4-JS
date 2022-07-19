964457aa92d143df2fdf8ee6498b057b3310a1a3
Lucas Duarte <lucasduartechaves@hotmail.com>
2022-07-19 20:29:16 -0300

    Adiciona evento no submit

--- form01.js
+++ form01.js
@@ -1,11 +1,17 @@
+document.exm01.addEventListener("submit", submitListener);
+
 function criaElemento(cor, largura, altura) {
   const eNovo = document.createElement("div");
   eNovo.textContent = `${cor} ${largura} ${altura}`;
   document.body.appendChild(eNovo);
 }
 
-criaElemento(
-  document.exm01.cor.value,
-  document.exm01.largura.value,
-  document.exm01.altura.value
-);
+function submitListener(event) {
+  event.preventDefault();
+
+  criaElemento(
+    document.exm01.cor.value,
+    document.exm01.largura.value,
+    document.exm01.altura.value
+  );
+}
